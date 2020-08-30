29a472c854b247617c23a8ba93bb1e2dc819f2c5
AlexsandrRybalko <rybalkosanya20@gmail.com>
2020-08-30 14:44:24 +0300

    added Item.cs

---
+++ Homework6ClassLibrary/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6ClassLibrary
+{
+    public partial class Item
+    {
+        public string Name { get; }
+        public double Price { get; set; }
+        public DateTime DeliveryDate { get; }
+
+        public Item(string name, double price, DateTime deliveryDate, int daysToExpire)
+        {
+            Name = name;
+            Price = price;
+            DeliveryDate = deliveryDate;
+            DaysToExpire = daysToExpire;
+        }
+
+        public DateTime GetExpirationDate()
+        {
+            return DeliveryDate.AddDays(DaysToExpire);
+        }
+    }
+}
